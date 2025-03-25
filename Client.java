1bb07d147def4984db8e2c191b3edcf25ef0757c
jakemoss127 <jacobmoss@ufl.edu>
2025-03-25 17:44:09 -0400

    Added file support to the Client

--- Client.java
+++ Client.java
@@ -21,11 +21,9 @@ public class Client {
             out = new DataOutputStream(socket.getOutputStream());
 
             System.out.println("Connection established at " + addr + " on PORT " + port);
-        }
-        catch (UnknownHostException err) {
+        } catch (UnknownHostException err) {
             System.out.println(err);
-        }
-        catch (IOException err) {
+        } catch (IOException err) {
             System.out.println(err);
         }
 
@@ -36,16 +34,19 @@ public class Client {
         // Read until "bye" is input
         while (!message.equals("bye")) {
             try {
+                // Read user input
+                System.out.print("Enter a file name: ");
+                message = in.readLine();
+                if (message == null)
+                    break;
                 if (message.equals("bye")) {
+                    out.writeUTF("bye");
+                    out.flush();
                     response = rin.readUTF();
                     System.out.println(response);
                     break;
                 }
-
-                // Read user input
-                System.out.print("Enter a string: ");
-                message = in.readLine();
-
+                
                 // Write to server + time it
                 long startTime = System.nanoTime();
 
@@ -54,16 +55,39 @@ public class Client {
 
                 // Read from server
                 response = rin.readUTF();
-                System.out.println(response);
+                if (response.equals("File not found")) {
+                    // The server didn't find the file
+                    System.out.println(response);
+                } else {
+                    // The server found the file
+                    long fileSize = rin.readLong();
+                    FileOutputStream fos = new FileOutputStream("./downloads/" + filename);
+                    byte[] buffer = new byte[4096];
+                    long totalBytesRead = 0;
+    
+                    while (totalBytesRead < fileSize) {
+                        int bytesRead = rin.read(buffer);
+                        if (bytesRead == -1) {
+                            break; 
+                            // something went wrong or server closed early
+                        }
+                        fos.write(buffer, 0, bytesRead);
+
+                        totalBytesRead += bytesRead;
+                    }
+
+                    fos.close();
+                    System.out.println("Downloaded: " + message + " (" + totalBytesRead + " bytes)");
+                }
 
                 long endTime = System.nanoTime();
-                double roundTripTime = (endTime - startTime) / 10000000.0;  // Convert to milliseconds
+                double roundTripTime = (endTime - startTime) / 10000000.0; // Convert to milliseconds
                 System.out.println("Round-trip time: " + roundTripTime + " ms");
                 roundTripTimes.add(roundTripTime);
 
-            }
-            catch (IOException err) {
+            } catch (IOException err) {
                 System.out.println(err);
+                break;
             }
         }
 
@@ -77,14 +101,12 @@ public class Client {
             socket.close();
 
             System.out.println("Connected closed.");
-        }
-        catch (IOException err) {
+        } catch (IOException err) {
             System.out.println(err);
         }
     }
 
-    public static void main(String args[])
-    {
+    public static void main(String args[]) {
         if (args.length != 2) {
             System.out.println("Usage: java Client <server_address> <port_number>");
             return;
